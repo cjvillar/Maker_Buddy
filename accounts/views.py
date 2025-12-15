981669f0de7927a94e068e9a9e68ca63fcf08b5d
Chris Villarreal <cjvillar@users.noreply.github.com>
2025-12-15 14:18:08 -0800

    added place holder views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_profile(request):
+    return render(request, "test.html", {
+        "title": "Accounts App"
+    })
--- public_feed/views.py
+++ public_feed/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def public_feed(request):
+    return render(request, "test.html", {
+        "title": "Accounts App"
+    })
