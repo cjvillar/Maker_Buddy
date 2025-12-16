db4de80afc136f1f7e6e98a7a2dd6c03f7d782c5
Chris Villarreal <cjvillar@users.noreply.github.com>
2025-12-16 11:34:01 -0800

    added css, project model/views,test

--- public_feed/views.py
+++ public_feed/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def public_feed(request):
-    return render(request, "navbar.html", {"title": "Accounts App"})
+    return render(request, "base.html", {"title": "Accounts App"})
