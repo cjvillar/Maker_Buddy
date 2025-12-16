fa6560100ea500c4932d7fd790fb8a425b8a3df3
Chris Villarreal <cjvillar@users.noreply.github.com>
2025-12-16 13:12:32 -0800

    updated css and view accounts

--- maker_projects/models.py
+++ maker_projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Project(models.Model):
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="maker_projects"
