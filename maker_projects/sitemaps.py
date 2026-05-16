db5549a782e42406a14a14ccd4898fe4ef9e2147
Chris Villarreal <cjvillar@users.noreply.github.com>
2026-05-15 22:09:58 -0700

    updated SEO

---
+++ maker_projects/sitemaps.py
@@ -0,0 +1,30 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from .models import MakerProject
+
+
+class ProjectSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = "https"
+
+    def items(self):
+        return MakerProject.objects.all()
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, obj):
+        return reverse("maker_projects:detail", kwargs={"pk": obj.pk})
+
+
+class StaticSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+    protocol = "https"
+
+    def items(self):
+        return ["public_feed:home"]
+
+    def location(self, item):
+        return reverse(item)
